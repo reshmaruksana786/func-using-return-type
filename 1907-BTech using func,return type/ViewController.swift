4ab62f32a5158544b9b53e557307151ecc2f6d43
BRN1907 <brn1907student@achiever-107.local>
2019-07-31 14:30:02 +0530

    Initial Commit

---
+++ 1907-BTech using func,return type/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1907-BTech using func,return type
+//
+//  Created by BRN1907 on 7/31/19.
+//  Copyright © 2019 BRN1907. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 1907-BTech using func,return typeTests/_907_BTech_using_func_return_typeTests.swift
@@ -0,0 +1,36 @@
+//
+//  _907_BTech_using_func_return_typeTests.swift
+//  1907-BTech using func,return typeTests
+//
+//  Created by BRN1907 on 7/31/19.
+//  Copyright © 2019 BRN1907. All rights reserved.
+//
+
+import XCTest
+@testable import _907_BTech_using_func_return_type
+
+class _907_BTech_using_func_return_typeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
